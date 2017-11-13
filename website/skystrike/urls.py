61c4c75af57ab5559f19ff9b4bf3cd6fddf2ad7e
ulimit65535 <2510823550@qq.com>
2017-11-13 10:21:35 +0800

    666

--- website/skystrike/urls.py
+++ website/skystrike/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^favicon.ico$',RedirectView.as_view(url=r'static/favicon.ico')),
     url(r'', include('warehouse.urls')),
 ]
